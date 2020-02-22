d3906c22239c93905df7023a6113a26f272458a3
chaos <zhangchaoyi13@outlook.com>
2020-02-22 22:34:10 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -10,8 +10,9 @@ index_page_size = 10
 archives_page_size = 20
 template = {
     "name": "Kepler",
-    "type": "local",
-    "path": "../Galileo"
+    "type": "git",
+    "url": "https://github.com/AlanDecode/Maverick-Theme-Kepler.git",
+    "branch": "latest"
 }
 enable_jsdelivr = {
     "enabled": True,
